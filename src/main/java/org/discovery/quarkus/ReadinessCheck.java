820dccc18c12d21531d3f590afc4cb8415d9232c
Davi Santos <davi.santos4357@gmail.com>
2026-01-13 19:02:27 -0300

    Rename

--- src/main/java/org/discovery/quarkus/ReadinessCheck.java
+++ src/main/java/org/discovery/quarkus/ReadinessCheck.java
@@ -7,7 +7,7 @@ import org.eclipse.microprofile.rest.client.inject.RestClient;
 import static org.discovery.quarkus.StarWarsService.FALLBACK_MESSAGE;
 
 @org.eclipse.microprofile.health.Readiness
-public class Readiness implements HealthCheck {
+public class ReadinessCheck implements HealthCheck {
 
     @RestClient
     StarWarsService starWarsService;
